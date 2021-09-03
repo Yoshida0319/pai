7ad0cfedf0508b931dfd9b8368156d5bb3a6cd9a
Yoshida0319 <88998401+Yoshida0319@users.noreply.github.com>
2021-09-03 21:42:28 +0900

    Create tamesi.js

---
+++ tamesi.js
@@ -0,0 +1,62 @@
+'use strict';
+        const a=document.getElementById('tamesi');
+        const b=document.getElementById('tamesi2');
+        const c=document.getElementById('botan');
+        const re=document.getElementById('result-area')
+        const pai=a.value;
+        b.onkeydown=event=>{
+         if(event.key==='Enter'){
+            c.onclick();
+         }
+        };
+        c.onclick = ()=>{
+            const x=b.value;
+            const l=x.length;
+            if (l===0) {
+                return;
+            }
+            re.innerText="";
+            const header=document.createElement('h4');
+            header.innerText='その数列は、小数点以下';
+            re.appendChild(header);
+            var r=[0];
+         for (let i = 0; i+l-1 < pai.length; i++) {
+            var d=pai[i];
+            var j=0;
+            fun();
+            function fun() {
+                let e=x[j];
+                let d=pai[i+j];
+             if (d===e && j<l) {
+                j++;
+                fun();
+             } else if(j===l && l===1){
+                const paragraph=document.createElement('p');
+                const result=(i+1 + '桁');
+                r.push(1);
+                paragraph.innerText=result;
+                re.appendChild(paragraph);
+             } else if (j===l && l>1){
+                let f=i+1;
+                let g=i+l;
+                const paragraph=document.createElement('p');
+                const result=(f + '桁~' + g +'桁');
+                r.push(1);
+                paragraph.innerText=result;
+                re.appendChild(paragraph);
+             } else if(i+l === pai.length && r[1] === 1){
+               const batu=document.createElement('h4');
+               const maru=('にあります');
+               batu.innerText=maru;
+               re.appendChild(batu);
+             } else if(i+l === pai.length) {
+               const batu=document.createElement('h4');
+               const maru=('50桁にはありませんでした');
+               batu.innerText=maru;
+               re.appendChild(batu);
+             } else{
+               return;
+             }
+            }
+            }
+        }
